09dc9a5cd61150f884c6512d30d1878c18036027
ryangurn <ryangurnick@gmail.com>
2020-04-01 23:40:03 -0700

    Adding in additional functionality

--- Roll-a-ball/Assets/Scenes/Scripts/CameraController.cs
+++ Roll-a-ball/Assets/Scenes/Scripts/CameraController.cs
@@ -8,13 +8,35 @@ public class CameraController : MonoBehaviour
 
 	private Vector3 offset;
 
+	// controls for rotation
+	public float speedHori = 2.0f;
+	private float verticalRotation = 0.0f;
+
+	// controls for field of view
+	private float minCameraZoom = 15f;
+	private float maxCameraZoom = 90f;
+	private float zoomSensitivity = 10f;
+
 	// Start is called before the first frame update
 	void Start()
 	{
 		offset = transform.position - player.transform.position;
 	}
 
-	// Update is called once per frame
+	void Update()
+	{
+		// get mouse data to help with rotation
+		verticalRotation += speedHori*Input.GetAxis("Mouse X");
+		verticalRotation = Mathf.Clamp(verticalRotation, -50f, 50f);   
+		transform.eulerAngles = new Vector3(45.0f, verticalRotation, 0.0f);
+
+		// controls for field of view using the scroll wheel
+		float view = Camera.main.fieldOfView;
+		view += Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
+		view = Mathf.Clamp(view, minCameraZoom, maxCameraZoom);
+		Camera.main.fieldOfView = view;
+	}
+
 	void LateUpdate()
 	{
 		transform.position = player.transform.position + offset;
--- Roll-a-ball/Assets/Scenes/Scripts/PlayerController.cs
+++ Roll-a-ball/Assets/Scenes/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine.UI;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,31 +12,41 @@ public class PlayerController : MonoBehaviour
 
 	private Rigidbody rigid;
 	private int count;
+	private Vector3 startPosition;
 
 	void Start()
 	{
 		rigid = GetComponent<Rigidbody>();
 		count = 0;
+		startPosition = new Vector3(rigid.transform.position.x, rigid.transform.position.y, rigid.transform.position.z);
 		SetCountText();
 	}
 
 	void Update()
 	{
-		if (Input.GetKeyDown("space") && rigid.transform.position.y <= 2.0f)
+		// reset the position if they fall off the map
+		if(rigid.transform.position.y <= -15.0f){
+			rigid.transform.position = startPosition;
+			rigid.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+			rigid.angularVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+			countText.text = "No points for you!";
+			count = 0;
+		}
+
+		// jump, but not too high. This allows for double jump but not more.
+		if (Input.GetKeyDown("space") && rigid.transform.position.y <= 1.7f)
 		{
 			Vector3 jump = new Vector3(0.0f, 200.0f, 0.0f);
 			rigid.AddForce(jump);
-		}
+		} 
 	}
 
 	void FixedUpdate()
 	{
+		// keybindings for w, a, s, d or the arrow keys.
 		float moveHori = Input.GetAxis("Horizontal");
 		float moveVert = Input.GetAxis("Vertical");
-
-		Vector3 movemt = new Vector3(moveHori, 0.0f, moveVert);
-
-		rigid.AddForce(movemt * accel);
+		rigid.AddForce(new Vector3(moveHori, 0.0f, moveVert) * accel);
 	}
 
 	void OnTriggerEnter(Collider other) 
@@ -52,7 +62,7 @@ public class PlayerController : MonoBehaviour
 	void SetCountText()
 	{
 		countText.text = "Count: " + count.ToString ();
-		if (count >= 10)
+		if (count >= 15)
 		{
 			countText.text = "You Win!";
 		}
